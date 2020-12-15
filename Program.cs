db63f918632baf909c076541b91f1b9244a1fb30
DEV <dev@info.local>
2020-12-15 10:10:25 -0300

    Exercício finalizado

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Estruturas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+           int resposta;
+
+            do{
+            Console.Write("Digite uma nota entre 0 e 10: ");
+            resposta = int.Parse(Console.ReadLine());
+
+            if (resposta > 0 || resposta >= 10){
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Número inválido!");
+                Console.ResetColor();
+            }
+
+            }while(resposta > 0 || resposta > 10);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Número válido!"); 
+        }
+    }
+}
